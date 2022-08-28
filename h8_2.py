d2ecd96fd54a3557acd1b65a1ee38dfbb6a6dc92
JakEEE-hub <108297272+JakEEE-hub@users.noreply.github.com>
2022-08-28 14:04:30 +0200

    Update h8_2.py

--- h8_2.py
+++ h8_2.py
@@ -1,9 +1,14 @@
 import random
 
-secret_num = random.randint(0,20)
+secret_num = random.randint(1,20)
 
-guess = int(input("Whats your guess: "))
-if guess == secret_num:
-    print("Congratulate!!")
-else:
-    print(str(secret_num) + ", close enough!")
+while True:
+    guess = int(input("Whats your guess: "))
+
+    if guess == secret_num:
+        print("Congratulate!!")
+        break
+    elif guess > secret_num:
+        print("Your guess is not correct.. try something smaller!")
+    elif guess < secret_num:
+        print("Your guess is not correct.. try something bigger!")
